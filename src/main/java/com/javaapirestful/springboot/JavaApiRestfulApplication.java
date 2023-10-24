dcc39bf32581d5e9e9f4172eb03b47f9a3d32849
JohnDaCruz <batatadoce31@hotmail.com>
2023-10-24 03:38:20 -0300

    Database Config

--- src/main/java/com/javaapirestful/springboot/JavaApiRestfulApplication.java
+++ src/main/java/com/javaapirestful/springboot/JavaApiRestfulApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JavaApiResTfulApplication {
+public class JavaApiRestfulApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(JavaApiResTfulApplication.class, args);
+        SpringApplication.run(JavaApiRestfulApplication.class, args);
     }
 }
