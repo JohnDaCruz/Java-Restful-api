c514870f0a5c725b7335d0b819960366a8bf1a45
JohnDaCruz <batatadoce31@hotmail.com>
2023-10-23 23:33:01 -0300

    Criação do Projeto em Spring Boot

---
+++ src/main/java/com/javaapirestful/springboot/JavaApiResTfulApplication.java
@@ -0,0 +1,13 @@
+package com.javaapirestful.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaApiResTfulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaApiResTfulApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/javaapirestful/springboot/JavaApiResTfulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaapirestful.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaApiResTfulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
